09244c04e272a90ee4a0e1a17ccb8a197357e5cd
Witcher0766 <kumarvishwajeet076@gmail.com>
2024-12-03 22:25:37 +0530

    components and pages addedd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import "./App.css";
+
+import { Route, Routes } from "react-router-dom";
+// import IndexPage from "./pages/indexpage/IndexPage";
+// import LoginPage from './pages/loginpage/LoginPage';
+// import Register from './pages/loginpage/Register';
+// import Create from './pages/create/Create';
+// import Postpage from './pages/postpage/Postpage';
+// import Editpost from './pages/editpost/Editpost';
+import Layout from "./component/layout/Layout";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {" "}
+      </Route>
+      {/* <Route index element={<IndexPage/>} /> */}
+    </Routes>
   );
 }
+      // <Route path="/login" element={<LoginPage />} />
+      // <Route path="/register" element={<Register />} />
+      // <Route path="/create" element={<Create />} />
+      // <Route path="/post/:id" element={<Postpage />} />
+      // <Route path="/edit/:id" element={<Editpost />} />
 
 export default App;
---
+++ frontend/src/component/header/Header.jsx
@@ -0,0 +1,77 @@
+import React, { useContext, useEffect } from 'react'
+import styles from './Header.module.css'
+import { Link, useNavigate } from 'react-router-dom'
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Box from '../box/Box';
+
+const Header = () => {
+  // const {setUserInfo, userInfo} = useContext(UserContext);
+  // let navigate = useNavigate();
+
+
+  // useEffect(() => {
+  //   fetch(`${process.env.REACT_APP_SERVER_URL}profile`, {
+  //     credentials: 'include',
+  //   }).then((response) => {
+  //     response.json().then(userInfo => {
+  //       setUserInfo(userInfo);
+  //     })
+  //   })
+  // }, []);
+
+  // function logout() {
+  //   fetch(`${process.env.REACT_APP_SERVER_URL}logout`, {
+  //     credentials: 'include',
+  //     method: 'POST',
+  //   })
+  //   .then(response => {
+  //     if (response.ok) {
+  //       setUserInfo(null);
+  //       navigate('/login');
+  //       toast.success("Logout successful");
+  //     } else {
+  //       toast.error("logout failed");
+  //     }
+  //   })
+  //   .catch(error => {
+  //     toast.error('Fetch error during logout:');
+  //   });
+  //   localStorage.clear();
+  // }
+
+  //   let username = userInfo?.username;
+  //   let firstLetter = username ? username.charAt(0).toUpperCase() : '';
+
+  function logout() {
+
+  }
+  const username = "testing"
+
+
+  return (
+    <>
+        <header>
+        <Link to="/" className='font-extrabold font-serif text-2xl md:text-3xl lg:text-4xl' >Bol<span className='text-sky-400'>ger</span></Link>
+        <nav>
+        {username && (
+          <>
+            <Link className={styles["link-btn"]} to="/create">Create</Link>
+            <button className='btn-1' onClick={logout}>Logout</button>
+            <p className='w-1/2 h-1/2 border-' to="/create">F</p>
+          </>
+        )}
+        {!username && (
+          <>
+          <Link className={styles["log-btn"]} to="/login">Login</Link>
+          <Link className={styles["log-btn1"]} to="/register">Register</Link>
+          </>
+        )}
+        </nav>
+      </header>
+    <Box/>
+    </>
+  )
+}
+
+export default Header;
