6d50060c3988b5f74d97f47aa1e575ba79894f3f
Witcher0766 <kumarvishwajeet076@gmail.com>
2024-12-23 23:03:39 +0530

    production

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running.....');
-})
+
 
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
@@ -27,6 +25,18 @@ app.use('/api/upload', uploadRoutes);
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running.....');
+    })
+}
 
 app.use(notFound);
 app.use(errorHandler);
